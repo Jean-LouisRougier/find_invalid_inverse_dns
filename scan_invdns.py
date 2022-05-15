410b9bac207f3af04423933ee8c2b38379108897
Jean-Louis Rougier <104377285+Jean-LouisRougier@users.noreply.github.com>
2022-05-15 10:11:50 +0200

    Added ping check / unused IP variable

--- scan_invdns.py
+++ scan_invdns.py
@@ -32,6 +32,7 @@ unvalid_ptr=0
 unverified_ptr=0
 total=0
 unused=0
+
 for ip in IPNetwork(str(sys.argv[1])): 
     #print(ip)
     total+=1
@@ -52,13 +53,21 @@ for ip in IPNetwork(str(sys.argv[1])):
                 print(ip,":",ans[0], "(unverified, DNS failed)")
                 unverified_ptr+=1
     except:
-        no_ptr+=1
-        if verbose==True:
-            print(ip, "no PTR found")
+        if pingOk(ip):
+            no_ptr+=1
+            if verbose==True:
+                print(ip, ": Unused (no PTR, no Ping)")
+        else:
+            unused+=1
+            if verbose==True:
+                print(ip, ": no PTR found")
+                
 print("*** Summary ***")
 print(total," addresses scanned")
 print("No PTR         ", 100.0*no_ptr/total, "%")
 print("Valid PTR      ", 100.0*valid_ptr/total, "%")
 print("Unvalid PTR    ", 100.0*unvalid_ptr/total, "%")
 print("Unverified PTR ", 100.0*unverified_ptr/total, "%")
+print("Unused         ", 100.0*unused/total, "%")
+
 
